358b319f5389b8179e221b84c96b157ec1065be2
Rafa Soler <rafasoler10@gmail.com>
2022-05-16 00:10:27 +0200

    Generate shortcode with latest announcements

--- includes/class-noticeboard.php
+++ includes/class-noticeboard.php
@@ -172,7 +172,7 @@ class Noticeboard {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
-
+		$this->loader->add_action( 'init', $plugin_public, 'register_shortcodes' );
 	}
 
 	/**
--- public/class-noticeboard-public.php
+++ public/class-noticeboard-public.php
@@ -100,4 +100,90 @@ class Noticeboard_Public {
 
 	}
 
+	/**
+	 * Register shortcodes
+	 *
+	 * @since    1.0.0
+	 */
+	function register_shortcodes() {
+		add_shortcode( 'latest_announcements', array( $this, 'shortcode_announcements_handler' ) );
+	}
+
+	/**
+	 * Generates the content of the latest announcements shortcode
+	 * 
+	 * @param array $atts Shortcode attributes
+	 * 
+	 * @return string
+	 */
+	function shortcode_announcements_handler( $atts ) {
+
+		$atts = shortcode_atts( array(
+			'limit' => 4,
+		), $atts, 'latest_announcements' );
+
+		$args = array(
+			'post_type' => 'nb_announcements',
+			'posts_per_page' => $atts['limit'],
+			'orderby' => 'date',
+			'order' => 'DESC',
+		);
+
+		$query = new WP_Query( $args );
+
+		$output = '<div class="announcements-list p-3">';
+
+		while ( $query->have_posts() ) {
+			$query->the_post();
+
+			$permalink = get_post_meta( get_the_ID(), 'announcement_link', true ) ?: get_permalink();
+			$permalink_text = get_post_meta( get_the_ID(), 'announcement_link_text', true );
+			$excerpt = get_post_meta( get_the_ID(), 'announcement_summary', true );
+
+			if ( ! $excerpt ) {
+				$excerpt = get_the_excerpt();
+				$excerpt = substr($excerpt, 0, 165); // Limit excerpt to 165 characters
+				$excerpt = substr($excerpt, 0, strrpos($excerpt, ' ')) . '...'; // Avoid breaking last word
+			}
+
+			ob_start();
+			?>
+
+			<article class="announcement" id="announcement-<?php the_ID(); ?>">
+
+				<header class="entry-header">
+					<h4 class="entry-title fs-6">
+						<a class="text-decoration-none fw-bold" href="<?= esc_url( $permalink ) ?>">
+							<?= get_the_title(); ?>
+						</a>
+					</h4>
+				</header>
+
+				<div class="entry-content">
+					<?= $excerpt ?>
+				</div>
+
+				<?php if ( $permalink_text ) : ?>
+					<footer class="entry-footer mt-2 text-end">
+						<a href="<?= esc_url( $permalink ) ?>" class="btn btn-sm btn-secondary link-primary fw-bold">
+							<?= get_post_meta( get_the_ID(), 'announcement_link_text', true ) ?>
+						</a>
+					</footer>
+				<?php endif; ?>
+
+			</article>
+
+			<?php
+			
+			$output .= ob_get_clean();
+
+		}
+
+		$output .= '</div>';
+
+		wp_reset_postdata();
+
+		return $output;
+	}
+
 }
